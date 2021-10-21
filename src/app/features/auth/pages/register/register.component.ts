571ccc061e392bce14adca4f769fe627742e793d
Dita Aprilani <ditapril@gmail.com>
2021-10-21 08:15:06 +0700

    register vendor UI. Ref vms#26

--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from "@angular/forms";
+
+import { TextBoxComponent } from "@progress/kendo-angular-inputs";
+import { LoginInterface } from 'src/app/core/interfaces/login-interface';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn: boolean = false;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedId() === 'true';
+    if (this.isLoggedIn) window.location.href = "/";
   }
 
+  constructor(private authService: AuthService) {}
+
+  public form: FormGroup = new FormGroup({
+    email: new FormControl(),
+    namaBadanUsaha: new FormControl(),
+    npwp: new FormControl(),
+    noTelp: new FormControl(),
+  });
+  
+  // login(): void {
+  //   this.form.markAllAsTouched();
+  //   let params: LoginInterface= {...this.form.value};
+  //   this.authService.login(params).subscribe(
+  //     (resp) =>  { 
+  //       this.isLoggedIn = true;
+  //       this.authService.setLoggedIn(true);
+  //       this.authService.setToken(resp.access_token)
+  //       window.location.href = "/"
+  //     },
+  //     (error) => { this.isLoggedIn = false }
+  //   );
+  // }
+
+  clearForm(): void {
+    this.form.reset();
+  }
 }
