a01a4bfeb354d25f78479e7a9cb340f39365a387
Dita Aprilani <ditapril@gmail.com>
2021-10-21 09:36:22 +0700

    register interface & button

---
+++ src/app/core/interfaces/register-interface.ts
@@ -0,0 +1,6 @@
+export interface RegisterInterface {
+    email: string;
+    namaBadanUsaha: string;
+    npwp: string;
+    noTelp: number;
+}
--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from "@angular/forms";
 
 import { TextBoxComponent } from "@progress/kendo-angular-inputs";
-import { LoginInterface } from 'src/app/core/interfaces/login-interface';
+import { RegisterInterface } from 'src/app/core/interfaces/register-interface';
 import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
@@ -28,19 +28,19 @@ export class RegisterComponent implements OnInit {
     noTelp: new FormControl(),
   });
   
-  // login(): void {
-  //   this.form.markAllAsTouched();
-  //   let params: LoginInterface= {...this.form.value};
-  //   this.authService.login(params).subscribe(
-  //     (resp) =>  { 
-  //       this.isLoggedIn = true;
-  //       this.authService.setLoggedIn(true);
-  //       this.authService.setToken(resp.access_token)
-  //       window.location.href = "/"
-  //     },
-  //     (error) => { this.isLoggedIn = false }
-  //   );
-  // }
+  register(): void {
+    this.form.markAllAsTouched();
+    let params: RegisterInterface= {...this.form.value};
+    // this.authService.login(params).subscribe(
+    //   (resp) =>  { 
+    //     this.isLoggedIn = true;
+    //     this.authService.setLoggedIn(true);
+    //     this.authService.setToken(resp.access_token)
+    //     window.location.href = "/"
+    //   },
+    //   (error) => { this.isLoggedIn = false }
+    // );
+  }
 
   clearForm(): void {
     this.form.reset();
