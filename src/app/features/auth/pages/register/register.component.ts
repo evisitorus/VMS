60c0dc9ca33b1eb7ab68a540cb5ca0ed40f5dcec
Dita Aprilani <ditapril@gmail.com>
2021-10-22 13:36:07 +0700

    update registration form refer to https://gitlab.playcourt.id/b2b-squad/padi-e-proc/vms/-/blob/master/LoFi-Mockup/Registrasi/1._Registrasi__-_Calon_Vendor.png Ref vms#26

--- src/app/features/auth/pages/register/register.component.ts
+++ src/app/features/auth/pages/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from "@angular/forms";
 
-import { TextBoxComponent } from "@progress/kendo-angular-inputs";
 import { RegisterInterface } from 'src/app/core/interfaces/register-interface';
 import { AuthService } from 'src/app/core/services/auth.service';
 
@@ -11,39 +10,33 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  // registerForm: FormGroup;
+  registerForm = new FormGroup({});
   submitted = false;
   isLoggedIn: boolean = false;
+  public mask: string = "99.999.999.9-999.999";
+  // public notelpMask: string = "000000000000000";
 
   constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
+
+  get f() { return this.registerForm.controls; }
 
   ngOnInit() {
     this.isLoggedIn = this.authService.isLoggedId() === 'true';
     if (this.isLoggedIn) window.location.href = "/";
 
-
-    this.form = this.formBuilder.group({
-      namaBadanUsaha: ['', Validators.required],
-      npwp: ['', [Validators.required, Validators.maxLength(20)]],
-      email: ['', [Validators.required, Validators.email]],
+    this.registerForm = this.formBuilder.group({
+      npwp: ['', [Validators.required]],
+      namaPerusahaan: ['', Validators.required],
       namaPic: ['', Validators.required],
-      noTelp: ['', [Validators.required,  Validators.maxLength(15)]]
+      email: ['', [Validators.required, Validators.email]],
+      noTelp: ['', [Validators.required,  Validators.maxLength(15), Validators.pattern('[0-9]+')]],
+      disclaimer: ['', Validators.requiredTrue]
     });
   }
-
-  public form: FormGroup = new FormGroup({
-    namaBadanUsaha: new FormControl(),
-    npwp: new FormControl(),
-    email: new FormControl(),
-    namaPic: new FormControl(),
-    noTelp: new FormControl(),
-  });
-
-  get f() { return this.form.controls; }
   
   register(): void {
-    this.form.markAllAsTouched();
-    let params: RegisterInterface= {...this.form.value};
+    this.registerForm.markAllAsTouched();
+    let params: RegisterInterface= {...this.registerForm.value};
     this.authService.register(params).subscribe(
       (resp) =>  { 
         this.submitted = true;
@@ -57,16 +50,16 @@ export class RegisterComponent implements OnInit {
     this.submitted = true;
 
     // stop here if form is invalid
-    if (this.form.invalid) {
+    if (this.registerForm.invalid) {
         return;
     }
 
     // display form values on success
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.form.value, null, 4));
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
   }
 
   onReset() {
       this.submitted = false;
-      this.form.reset();
+      this.registerForm.reset();
   }
 }
