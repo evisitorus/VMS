bb68d3ee18f9e2ec08f5afefb674f52d11fd02f1
Gian Permana <gianpermana0805@gmail.com>
2021-10-27 01:40:12 +0000

    vms#29 update login pop up message function

--- src/app/core/services/api/api-routes.ts
+++ src/app/core/services/api/api-routes.ts
@@ -1,4 +1,6 @@
-const API_BASE_URL = "http://192.168.1.102:8000";
+import { environment as env} from "src/environments/environment";
+
+const API_BASE_URL = env.api_base_path;
 
 export const ApiRoutes = {
     api_login_route: API_BASE_URL + "/auth/login"
--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -6,15 +6,6 @@ import { LoginInterface } from 'src/app/core/interfaces/login-interface';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { EventEmitterService } from 'src/app/core/services/event-emitter.service';
 
-const messages = {
-  success: 'Selamat Datang "PT. Abadi Jaya Sentosa Selalu" di Vendor Management System (VMS) PaDi',
-  wrong_email: 'Email tidak terdaftar, silahkan inputkan email benar atau lakukan registrasi jika anda belum memiliki akun',
-  wrong_password: 'Password yang anda inputkan salah, silahkan ulangi!", #maximum input wrong password 3x',
-  locked_account: 'Akun anda terkunci, silahkan hubungi Admin di admin@vms-padi.co.id untuk melakukan recovery akun',
-  reset_password: 'Silahkan klik reset password untuk mengatur password baru anda',
-  unidentified: 'User tidak dikenali, silahkan inputkan email dan password yang benar atau lakukan registrasi jika anda belum memiliki akun !'
-};
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -27,7 +18,7 @@ export class LoginComponent implements OnInit{
 
   isLoggedIn: boolean = false;
   popUpTitle: string = "Informasi Login";
-  popUpMessage: string = messages.success;
+  popUpMessage: string = "";
   redirectOnClosePopUp: boolean = true;
 
   constructor(
@@ -61,13 +52,14 @@ export class LoginComponent implements OnInit{
       (resp) =>  { 
         this.isLoggedIn = true;
         this.authService.setLoggedIn(true);
-        this.authService.setToken(resp.access_token);
+        this.authService.setToken(resp.data.access_token);
+        this.popUpMessage = resp.message;
         this.redirectOnClosePopUp = true;
         this.triggerPopUp();
       },
       (error) => { 
         this.isLoggedIn = false;
-        this.popUpMessage = error.statusText;
+        this.popUpMessage = error.error.message;
         this.redirectOnClosePopUp = false;
         this.triggerPopUp();
       }
