d2425131d45d618fef2ce744bb04a569e54b5749
Dita Aprilani <ditapril@gmail.com>
2021-11-03 06:02:34 +0700

    static grid. Ref vms#42

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './layouts/main/main.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
+import { CardComponent } from './layouts/card/card.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -16,6 +17,8 @@ import { CoreModule } from './core/core.module';
 import { AuthModule } from './features/auth/auth.module';
 import { ProfileModule } from './features/profile/profile.module';
 import { DialogsModule } from '@progress/kendo-angular-dialog';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+
 
 
 
@@ -25,6 +28,7 @@ import { DialogsModule } from '@progress/kendo-angular-dialog';
     MainComponent,
     FooterComponent,
     NavbarComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,6 +43,7 @@ import { DialogsModule } from '@progress/kendo-angular-dialog';
 
     AppRoutingModule,
     DialogsModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
