530b256d94f58d7c3b75451c07274b165f802aa9
Tomi Putro Utomo <tomi.putro.utomo@gmail.com>
2021-11-10 09:42:06 +0700

    vms#49 [FE] connect profile pic to backend done. however, password section still buggy and validation error message not shown at angular yet

--- src/app/core/services/api/api-routes.ts
+++ src/app/core/services/api/api-routes.ts
@@ -2,19 +2,19 @@ const API_BASE_URL = "https://localhost";
 import { environment as env} from "src/environments/environment";
 
 export const ApiRoutes = {
-    api_login_route: API_BASE_URL + "/auth/login",
-    api_register_route: API_BASE_URL + "/users/register",
-    api_activate_route: API_BASE_URL + "/users/activate",
-    api_forgot_password_route: API_BASE_URL + "/auth/forgot-password",
-    api_reset_password_route: API_BASE_URL + "/auth/reset-password",
-    api_dashboard_vendor_route: API_BASE_URL + "/users/dashboard/1"
+  api_login_route: API_BASE_URL + "/auth/login",
+  api_register_route: API_BASE_URL + "/users/register",
+  api_activate_route: API_BASE_URL + "/users/activate",
+  api_forgot_password_route: API_BASE_URL + "/auth/forgot-password",
+  api_reset_password_route: API_BASE_URL + "/auth/reset-password",
+  api_dashboard_vendor_route: API_BASE_URL + "/users/dashboard/1",
+  api_profile_pic: API_BASE_URL + "/users/profile/pic/26",
 };
 
 
-
 export const ApiRouteMethods = {
-    post: 'POST',
-    get: 'GET',
-    put: 'PUT',
-    delete: 'DELETE'
+  post: 'POST',
+  get: 'GET',
+  put: 'PUT',
+  delete: 'DELETE'
 };
