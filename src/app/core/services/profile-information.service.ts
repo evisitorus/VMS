b9348e2f13ee5ed96772f6be2b7c7e91489cba7d
evadantir <evadantir@yahoo.com>
2021-11-15 15:06:17 +0700

    create function to get and using token auth vms#46

--- src/app/core/services/profile-information.service.ts
+++ src/app/core/services/profile-information.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { ApiInterface } from '../interfaces/api-interface';
 import { ApiRouteMethods, ApiRoutes } from './api/api-routes';
 import { ApiService } from './api/api.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,23 @@ import { ApiService } from './api/api.service';
 export class ProfileInformationService {
 
   constructor(
-    private apiService:ApiService
+    private apiService:ApiService,
+    private authService: AuthService
   ) { }
 
   uploadImg(img: String): Observable<any> {
+    let token = this.authService.getLocalStorage('access_token')!;
     let api_profile_information: ApiInterface = {
       method: ApiRouteMethods.post,
-      url: ApiRoutes.api_profile_information,
+      // url: ApiRoutes.api_profile_information,
+      url:"",
       body : {
         img: img
+      },
+      options : {
+        headers: {
+          Authorization: token
+        }
       }
     }
     return this.apiService.sendRequest(api_profile_information);
