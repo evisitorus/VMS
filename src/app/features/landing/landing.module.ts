c78e178316a5bea62df3dd7975220ee394e03a88
Gian Permana <gianpermana0805@gmail.com>
2021-11-15 16:30:43 +0700

    vms#40 Date Formating

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
@@ -8,6 +8,9 @@ import { TenderInfoComponent } from './pages/tender-info/tender-info.component';
 import { CoreModule } from 'src/app/core/core.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import localeId from '@angular/common/locales/id'; 
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeId, 'id'); 
 
 @NgModule({
   declarations: [
@@ -23,6 +26,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
   ],
   exports: [
     LandingPageComponent,
-  ]
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: "id-ID" }]
 })
 export class LandingModule { }
