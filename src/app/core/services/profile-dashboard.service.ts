8b846d4a79f7f92585ce825ce49277ee40ce1c3f
evadantir <evadantir@yahoo.com>
2021-11-16 21:18:30 +0700

    get token vms#73

--- src/app/core/services/profile-dashboard.service.ts
+++ src/app/core/services/profile-dashboard.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { ApiInterface } from '../interfaces/api-interface';
 import { ApiRoutes,ApiRouteMethods } from './api/api-routes';
 import { ApiService } from './api/api.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +11,24 @@ import { ApiService } from './api/api.service';
 export class ProfileDashboardService {
 
   constructor(
-    private apiService:ApiService
+    private apiService:ApiService,
+    private authService: AuthService
   ) { }
 
+  token = this.authService.getLocalStorage('access_token')!;
   getVendorData(): Observable<any> {
     let api_dashboard_vendor: ApiInterface = {
       method: ApiRouteMethods.get,
-      url: ApiRoutes.api_dashboard_vendor_route
+      url: ApiRoutes.api_dashboard_vendor_route,
+      options : {
+        headers: {
+          Authorization: this.token
+        }
+      }
     }
 
     return this.apiService.sendRequest(api_dashboard_vendor);
   }
-  
+
+ 
 }
