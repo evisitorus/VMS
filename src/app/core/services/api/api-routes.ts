308c3de385bed7278831e51d0abee4c1605d012a
evadantir <evadantir@yahoo.com>
2021-11-17 11:20:45 +0700

    add new routes vms#46

--- src/app/core/services/api/api-routes.ts
+++ src/app/core/services/api/api-routes.ts
@@ -7,12 +7,14 @@ export const ApiRoutes = {
   api_activate_route: API_BASE_URL + "/users/activate",
   api_forgot_password_route: API_BASE_URL + "/auth/forgot-password",
   api_reset_password_route: API_BASE_URL + "/auth/reset-password",
-  api_dashboard_vendor_route: API_BASE_URL + "/users/1",
+  api_dashboard_vendor_route: API_BASE_URL + "/vendors/1",
   api_profile_pic: API_BASE_URL + "/users/profile/pic/26",
   api_add_pengalaman_kerja: API_BASE_URL + "/badan_usahas/pengalaman_kerja",
   api_get_pengalaman_kerja: API_BASE_URL + "/pengalaman_kerjas",
   api_assets_route: API_BASE_URL + "/mesin_peralatans",
   api_penyedia_usaha_route : API_BASE_URL + "/jenis_penyedia_usahas?page=1",
+  api_jenis_badan_usaha_route: API_BASE_URL + "",
+  api_jenis_vendor_route: API_BASE_URL + "jenis_vendors?page=1"
 };
 
 export const ApiRouteMethods = {
