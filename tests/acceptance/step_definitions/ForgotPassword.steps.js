e685562ad805bc02af5a8285623dfd73352e5502
evifebiana <evifebiana@gmail.com>
2021-11-22 15:07:24 +0700

    forgot password done auto test. vms#100

--- tests/acceptance/step_definitions/ForgotPassword.steps.js
+++ tests/acceptance/step_definitions/ForgotPassword.steps.js
@@ -18,6 +18,11 @@ Given('The Vendor will brings to {string} form to type his or her registered ema
     I.fillField('#input-email input[class=k-input]', 'vms@tmpbox.net');
 });
 
+Given('The Vendor clicks {string} button', () => {
+    I.click('#btn-reset-password');
+    I.amOnPage('/reset-password?token=4ccb494a00b1ed3f0cce56fc6c0002420eeb4d7f9358d69076994f425ec0dc89');
+});
+
 Given('The Vendor should be define his or her new password on {string} Form', () => {
     I.waitForElement('#input-new-password input[class=k-input]');
     I.fillField('#input-new-password input[class=k-input]', 'W@rMachineX123');
@@ -27,6 +32,11 @@ Given('The Vendor should be define his or her new password on {string} Form', ()
 
 Given('The Vendor will brings to {string} Form', () => {
     I.amOnPage('/login');
+});
+
+Given('The Vendor selects {string} button', () => {
+    I.waitForElement('#btn-reset-password');
+    I.click('#btn-reset-password');
 });
 
 Given('The Vendor can test his or her new password on {string} form while they can be enter into the app', () => {
