66d12e440699f50e2dcb151810f8a5817c3f1d3f
evifebiana <evifebiana@gmail.com>
2021-11-23 15:43:46 +0700

    Landing Page Tender Information (tender list) Done. vms#96

--- tests/acceptance/step_definitions/LandingPageTenderInformation.steps.js
+++ tests/acceptance/step_definitions/LandingPageTenderInformation.steps.js
@@ -26,7 +26,11 @@ Given('The User will see first ten list of Tenders which conduct by BUMN on {str
 });
 
 Given('The User can move to another page by clicking "pagination button"  to next 10 list of Tenders', () => {
-    I.seeElement('.k-link k-pager-nav');
+    I.seeElement('button.k-pager-first');
+    I.seeElement('button.k-pager-last');
+    I.seeElement('button.k-link.k-pager-nav span.k-i-arrow-e');
+    I.seeElement('button.k-link.k-pager-nav span.k-i-arrow-w');
+    I.seeElement('ul.k-pager-numbers li');
 });
 
 Given('The User can see list of Tender on {string}', () => {
