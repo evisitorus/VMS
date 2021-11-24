85907d0bad4b07c2f210f1107e242cef9632141d
Dita Aprilani <ditapril@gmail.com>
2021-11-24 09:04:42 +0700

    split component Ref vms#47

--- src/app/features/profile/pages/profile-informasi-perusahaan/data-pegawai/data-pegawai.component.ts
+++ src/app/features/profile/pages/profile-informasi-perusahaan/data-pegawai/data-pegawai.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Item {
+  name: string;
+  id: number;
+}
+
 @Component({
   selector: 'app-data-pegawai',
   templateUrl: './data-pegawai.component.html',
@@ -10,6 +15,30 @@ export class DataPegawaiComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public isRequired = true;
+  public opened = false;
+  public openedSaham = false;
+  public listItems: Array<Item> = [];
+
+
+  public close() {
+    console.log(`Dialog result: ${status}`);
+    this.opened = false;
+  }
+
+  public open() {
+    this.opened = true;
+  }
+
+  public closeSaham() {
+    console.log(`Dialog result: ${status}`);
+    this.openedSaham = false;
+  }
+
+  public openSaham() {
+    this.openedSaham = true;
   }
 
 }
