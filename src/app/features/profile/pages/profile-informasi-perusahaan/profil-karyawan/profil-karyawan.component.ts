d28468417a7303e3b55c79f5d9d3ad1aea9ffeec
evadantir <evadantir@yahoo.com>
2021-11-25 12:00:50 +0700

    assign data to field and column vms#47

--- src/app/features/profile/pages/profile-informasi-perusahaan/profil-karyawan/profil-karyawan.component.ts
+++ src/app/features/profile/pages/profile-informasi-perusahaan/profil-karyawan/profil-karyawan.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataBindingDirective } from '@progress/kendo-angular-grid';
 import { FileRestrictions } from '@progress/kendo-angular-upload';
 import { FileService } from 'src/app/core/services/file.service';
 
@@ -18,7 +19,51 @@ const messages = {
   styleUrls: ['./profil-karyawan.component.css']
 })
 export class ProfilKaryawanComponent implements OnInit {
-  @ViewChild('panelbar') private panelbar: any;
+  @ViewChild(DataBindingDirective)
+  dataBinding!: DataBindingDirective;
+
+  public columns = [
+    {field: "nik", title:"NIK"}, 
+    {field: "nama_pegawai", title:"Nama Pegawai"}, 
+    {field: "tipe_karyawan", title:"Tipe Karyawan"}, 
+    {field: "jabatan", title:"Jabatan"},
+    {field: "bidang_pekerjaan", title:"Bidang Pekerjaan"}, 
+    // {field: "resume", title:"Resume"}, 
+  ];
+
+  public dataKaryawan = [
+    {
+      nik: "848e6002-8a92-447d-951b-1ffd5e695578",
+      nama_pegawai: "Sig Jeannel",
+      jabatan: "Human Resources Assistant III",
+      tipe_karyawan: 1,
+      bidang_pekerjaan: "HR"
+    },
+    {
+      id: "19d18d40-0e64-4837-9420-92130a0ed253",
+      nama_pegawai: "Shelden Greyes",
+      jabatan: "Operator",
+      tipe_karyawan: 2,
+      bidang_pekerjaan: "Engineering"
+    },
+    {
+      nik: "bebdc6eb",
+      nama_pegawai: "Megen Cody",
+      jabatan: "Operator",
+      tipe_karyawan: 3,
+      bidang_pekerjaan: "Engineering"
+    },
+    {
+      nik: "38b08b88",
+      nama_pegawai: "Clevey Thursfield",
+      jabatan: "VP Quality Control",
+      tipe_karyawan: 2,
+      bidang_pekerjaan: "Engineering"
+    }
+  ];
+  
+  public gridData = this.dataKaryawan;
+  public gridView!: any[];
 
   popUpTitle: string = "Informasi Pemegang Saham";
   popUpMessage: string = messages.default;
@@ -50,40 +95,9 @@ export class ProfilKaryawanComponent implements OnInit {
     allowedExtensions: ["pdf", "doc", "docx"],
     maxFileSize: 20000000 //20 MB
   };
-  
-
-  public dataKaryawan = [
-    {
-      nik: "848e6002-8a92-447d-951b-1ffd5e695578",
-      nama: "Sig Jeannel",
-      jabatan: "Human Resources Assistant III",
-      tipe_karyawan_id: 1,
-      bidang_pekerjaan: "HR"
-    },
-    {
-      id: "19d18d40-0e64-4837-9420-92130a0ed253",
-      nama: "Shelden Greyes",
-      jabatan: "Operator",
-      tipe_karyawan_id: 2,
-      bidang_pekerjaan: "Engineering"
-    },
-    {
-      nik: "bebdc6eb",
-      nama: "Megen Cody",
-      jabatan: "Operator",
-      tipe_karyawan_id: 3,
-      bidang_pekerjaan: "Engineering"
-    },
-    {
-      nik: "38b08b88",
-      nama: "Clevey Thursfield",
-      jabatan: "VP Quality Control",
-      tipe_karyawan_id: 2,
-      bidang_pekerjaan: "Engineering"
-    }
-  ];
 
   ngOnInit(): void {
+    this.gridView = this.gridData;
   }
 
   public close() {
