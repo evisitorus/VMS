9aea5c84ce9a310589fe07f1b4c9886cb9e68d61
Dita Aprilani <ditapril@gmail.com>
2021-11-25 17:18:12 +0700

    id sidebar

--- src/app/features/profile/components/sidebar/sidebar.component.ts
+++ src/app/features/profile/components/sidebar/sidebar.component.ts
@@ -34,26 +34,27 @@ export class SidebarComponent implements OnInit {
 const items = [
   { 
     path: "Dashboard",
-    route:"/dashboard" 
+    route:"/dashboard",
+    id: "sidebar-dashboard" 
   },
   { 
     path: "Kelola Akun",
     children: [
-      { path: "Profil Perusahaan", route:"/profile-information" },
-      { path: "PIC", route:"/profile-person-in-charge" },
-      { path: "Dokumen", route:"/profile-dokumen" },
-      { path: "Alamat", route:"/profile-alamat" },
-      { path: "Laporan Keuangan", route:"/profile-laporan-keuangan" },
-      { path: "Riwayat Pekerjaan" , route:"/profile-riwayat-pekerjaan" },
-      { path: "Asset", route:"/profile-aset" },
-      { path: "Verifikasi Kelengkapan", route:"/profile-verification" },
+      { path: "Profil Perusahaan", route:"/profile-information", id: "sidebar-dashboard"  },
+      { path: "PIC", route:"/profile-person-in-charge", id: "sidebar-pic"  },
+      { path: "Dokumen", route:"/profile-dokumen", id: "sidebar-dokumen"  },
+      { path: "Alamat", route:"/profile-alamat", id: "sidebar-alamat"  },
+      { path: "Laporan Keuangan", route:"/profile-laporan-keuangan", id: "sidebar-laporan-keuangan"  },
+      { path: "Riwayat Pekerjaan" , route:"/profile-riwayat-pekerjaan", id: "sidebar-riwayat-pekerjaan"  },
+      { path: "Asset", route:"/profile-aset", id: "sidebar-aset"  },
+      { path: "Verifikasi Kelengkapan", route:"/profile-verification", id: "sidebar-verification"  },
       { path: " "}
     ]
   },
   { 
     path: "Tender",
     children: [
-      { path: "Informasi Tender BUMN" }
+      { path: "Informasi Tender BUMN", route:"/", id: "sidebar-tender"  }
     ]
   },
 ];
