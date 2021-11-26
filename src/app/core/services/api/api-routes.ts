84617dad69952af51ab60634d842a5a68bf5d635
Dita Aprilani <ditapril@gmail.com>
2021-11-26 12:19:28 +0700

    riwayat pekerjaan

--- src/app/core/services/api/api-routes.ts
+++ src/app/core/services/api/api-routes.ts
@@ -9,7 +9,7 @@ export const ApiRoutes = {
     api_activate_route: API_BASE_URL + "/users/activate",
     api_forgot_password_route: API_BASE_URL + "/auth/forgot-password",
     api_reset_password_route: API_BASE_URL + "/auth/reset-password",
-    api_add_pengalaman_kerja: API_BASE_URL + "/badan_usahas/pengalaman_kerja",
+    api_add_pengalaman_kerja: API_BASE_URL + "/vendors/pengalaman_kerja",
     api_get_pengalaman_kerja: API_BASE_URL + "/pengalaman_kerjas",
     api_dashboard_vendor_route: API_BASE_URL + "/users/dashboard/1",
     api_profile_pic: API_BASE_URL + "/users/profile/pic/26",
