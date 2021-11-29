4371b4b02077587f7d52bf54a8884d3c79374b47
evadantir <evadantir@yahoo.com>
2021-11-29 16:24:41 +0700

    move content to tentang kami component vms#123

--- src/app/layouts/tentang-kami/tentang-kami.component.ts
+++ src/app/layouts/tentang-kami/tentang-kami.component.ts
@@ -11,5 +11,44 @@ export class TentangKamiComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  public card1 = "./assets/images/card1.png";
+  public card2 = "./assets/images/card2.png";
+  public card3 = "./assets/images/card3.png";
+  public card4 = "./assets/images/card4.png";
+
+  public mediaCards: Array<any> = [
+    {
+      imageSrc: this.card1,
+      title:
+        "Governance, Transaparansi dan Digital Procurement",
+      description:
+        "Digitalisasi proses procurement agar procurement lebih transparan.",
+      actionsLayout: "start",
+    },
+    {
+      imageSrc: this.card2,
+      title:
+        "Efisiensi dan Sinergi Berkelanjutan",
+      description:
+        "Efisiensi dan sinergi dari proses procurement BUMN.",
+      actionsLayout: "start",
+    },
+    {
+      imageSrc: this.card3,
+      title:
+        "Mendorong Pemberdayaan UMKM",
+      description:
+        "Mendorong dan membuka kesempatan bagi para pelaku Usaha Mikro, Kecil, Menengah dalam prooses procurement di BUMN.",
+      actionsLayout: "start",
+    },
+    {
+      imageSrc: this.card4,
+      title:
+        "Optimalisasi TKDN",
+      description:
+        "Meningkatkan Tingkat Kandungan Dalam Negri (TKDN) dalam proses procurement di BUMN.",
+      actionsLayout: "start",
+    },
+  ];
 
 }
