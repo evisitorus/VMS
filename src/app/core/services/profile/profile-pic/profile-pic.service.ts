5e0b45026ffaa6059b265350b222131c341db384
Tomi Putro Utomo <tomi.putro.utomo@gmail.com>
2021-11-29 16:02:08 +0700

    vms#49 upload pic image done

--- src/app/core/services/profile/profile-pic/profile-pic.service.ts
+++ src/app/core/services/profile/profile-pic/profile-pic.service.ts
@@ -4,6 +4,8 @@ import {ApiInterface} from '../../../interfaces/api-interface';
 import {ApiRoutes, ApiRouteMethods} from "../../api/api-routes";
 import {ApiService} from "../../api/api.service";
 import {ProfilePICInterface} from "../../../interfaces/profile/profile-pic-interface";
+import {AuthService} from "../../auth.service";
+import {UserFileInterface} from "../../../interfaces/profile/user-file-interface";
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +13,26 @@ import {ProfilePICInterface} from "../../../interfaces/profile/profile-pic-inter
 export class ProfilePICService {
 
   constructor(
-    private apiService: ApiService
+    private apiService: ApiService,
+    private authService: AuthService
   ) {
   }
 
-  getProfilePIC(): Observable<any> {
+  getProfilePIC(personID: string): Observable<any> {
+    let person_id = this.authService.getLocalStorage('person_id')!;
     let api_profile_pic: ApiInterface = {
       method: ApiRouteMethods.get,
-      url: ApiRoutes.api_profile_pic
+      url: ApiRoutes.api_profile_pic.concat("/").concat(person_id),
     }
 
     return this.apiService.sendRequest(api_profile_pic);
   }
 
-  updateProfilePIC(params: ProfilePICInterface): Observable<any> {
+  updateProfilePIC(params: ProfilePICInterface, personID: string): Observable<any> {
+    let person_id = this.authService.getLocalStorage('person_id')!;
     let api_profile_pic: ApiInterface = {
       method: ApiRouteMethods.put,
-      url: ApiRoutes.api_profile_pic,
+      url: ApiRoutes.api_profile_pic.concat("/").concat(person_id),
       body: {
         name: params.name,
         email: params.email,
@@ -35,6 +40,24 @@ export class ProfilePICService {
         old_password: params.oldPassword,
         new_password: params.newPassword,
         confirm_new_password: params.confirmNewPassword,
+      },
+      options: {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      },
+    }
+
+    return this.apiService.sendRequest(api_profile_pic);
+  }
+
+  updateUserFile(userFileParam: UserFileInterface, personID: string): Observable<any> {
+    let person_id = this.authService.getLocalStorage('person_id')!;
+    let api_profile_pic: ApiInterface = {
+      method: ApiRouteMethods.put,
+      url: ApiRoutes.api_profile_pic.concat("/").concat(person_id).concat("/media"),
+      body: {
+        file_id_sting: userFileParam.fileID,
       },
       options: {
         headers: {
