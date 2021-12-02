09083072247e3e5ae625fdb308351c2fd44d9a89
Gian Permana <gianpermana0805@gmail.com>
2021-12-02 12:05:48 +0700

    vms#123 Minor Restyling Landing Page

--- src/app/features/landing/components/grid-logo/grid-logo.component.ts
+++ src/app/features/landing/components/grid-logo/grid-logo.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid-logo.component.css']
 })
 export class GridLogoComponent implements OnInit {
-
+  
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-
+  
+  public mediaCards: Array<any> = [
+    {
+      imageSrc: "./assets/images/landing/pnm.png",
+      last: false
+    },
+    {
+      imageSrc: "./assets/images/landing/pegadaian.png",
+      last: false
+    },
+    {
+      imageSrc: "./assets/images/landing/pln.png",
+      last: false
+    },
+    {
+      imageSrc: "./assets/images/landing/telkom.png",
+      last: false
+    },
+    {
+      imageSrc: "./assets/images/landing/pertamina.png",
+      last: true
+    }
+  ];
+  
 }
