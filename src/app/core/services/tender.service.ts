52ec2c3b8f9517427537b9a4e690b6c678bc2ceb
Dita Aprilani <ditapril@gmail.com>
2021-12-13 07:10:17 +0700

    fix auth tender

--- src/app/core/services/tender.service.ts
+++ src/app/core/services/tender.service.ts
@@ -3,28 +3,17 @@ import { Observable } from 'rxjs';
 import { ApiInterface } from '../interfaces/api-interface';
 import { ApiRouteMethods, ApiRoutes } from './api/api-routes';
 import { ApiService } from './api/api.service';
-import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
 export class TenderService {
 
-  constructor(
-    private apiService: ApiService,
-    private authService: AuthService
-  ) { }
-  
-  token = this.authService.getLocalStorage('access_token')!;
+  constructor(private apiService: ApiService) { }
 
   getListTender(query: string, page: number, per_page: number = 10): Observable<any> {    
     let api_list_tender: ApiInterface = {
       method: ApiRouteMethods.get,
-      url: ApiRoutes.api_tenders_route + "?page=" + page + "&per_page=" + per_page + "&" + query,
-      options: {
-        headers: {
-          Authorization: this.token
-        }
-      },
+      url: ApiRoutes.api_tenders_route + "?page=" + page + "&per_page=" + per_page + "&" + query
     };
     return this.apiService.sendRequest(api_list_tender);
   }
@@ -33,11 +22,6 @@ export class TenderService {
     let request: ApiInterface = {
       method: ApiRouteMethods.get,
       url: ApiRoutes.api_tenders_route.concat('/').concat(id),
-      options: {
-        headers: {
-          Authorization: this.token
-        }
-      },
     };
     return this.apiService.sendRequest(request);
   }
