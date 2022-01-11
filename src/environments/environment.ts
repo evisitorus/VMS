6117807c7c46265acd288ae4a987177e125f01e6
evifebiana <evifebiana@gmail.com>
2022-01-11 09:58:32 +0700

    add environment.local.ts and define to gitignore. vms#176

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,6 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-//for QA testing
-// export const environment = {
-//   production: false,
-//   api_base_path: "http://172.20.130.132:8000",
-//   acceptance_test : true
-// };
-
-
 export const environment = {
   production: false,
   api_base_path: "http://localhost:8000",
