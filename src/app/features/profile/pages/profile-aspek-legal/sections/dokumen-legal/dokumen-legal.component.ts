2352a24d782209a12011b59899e3015bfa2e625e
evadantir <evadantir@yahoo.com>
2022-03-24 11:25:35 +0700

    get dokumen legal data vms#365

--- src/app/features/profile/pages/profile-aspek-legal/sections/dokumen-legal/dokumen-legal.component.ts
+++ src/app/features/profile/pages/profile-aspek-legal/sections/dokumen-legal/dokumen-legal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FileRestrictions } from '@progress/kendo-angular-upload';
+import { ProfileAspekLegalService } from 'src/app/core/services/profile/profile-aspek-legal.service';
 import { ProfileAspekLegalComponent } from '../../profile-aspek-legal.component';
 
 @Component({
@@ -14,11 +16,50 @@ export class DokumenLegalComponent implements OnInit {
   private messages = {
     failed: "Gagal menemukan data dokumen",
   };
+  public fileRestrictions: FileRestrictions = {
+    allowedExtensions: ["jpg", "jpeg", "png", "pdf"],
+    maxFileSize: 2097152
+  };
+  public lampiranFiles!: Array<any>;
+
   constructor(
-    public parent: ProfileAspekLegalComponent
+    public parent: ProfileAspekLegalComponent,
+    private profileAspekLegalService: ProfileAspekLegalService,
   ) { }
 
   ngOnInit(): void {
+    this.fetchData();
+  }
+
+  public fetchData(): void {
+    this.profileAspekLegalService.getDocType().subscribe(
+      (response) => {
+        this.gridData = response['hydra:member'];
+        this.gridData = this.mapData(this.gridData);
+
+      },
+      (err) => {
+        this.parent.popUpMessage = err.error.message;
+        this.parent.triggerPopUp();
+      }
+    );
+  }
+
+  public mapData(data: any[]): any[] {
+    let mappedData: any[] = [];
+    let amount_to_remove = 2;
+    for (const key in data) {
+      mappedData[key] = {
+        id: data[key]['id'],
+        name: data[key]['name'],
+        mandatory: data[key]['isMandatory']
+      };
+    }
+
+    // get data only unit PKP
+    mappedData.splice(mappedData.length - amount_to_remove,amount_to_remove);
+    
+    return mappedData;
   }
 
 }
