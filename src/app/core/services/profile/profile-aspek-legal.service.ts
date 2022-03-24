c4c25ce715df0e8139528c788114af0cd2c10df1
evadantir <evadantir@yahoo.com>
2022-03-24 10:31:10 +0700

    add service for adding new aspek legal vms#365

--- src/app/core/services/profile/profile-aspek-legal.service.ts
+++ src/app/core/services/profile/profile-aspek-legal.service.ts
@@ -3,6 +3,8 @@ import { ApiService } from '../api/api.service';
 import { AuthService } from '../auth.service';
 import { ApiRouteMethods, ApiRoutes } from '../api/api-routes';
 import { ApiInterface } from '../../interfaces/api-interface';
+import { AspekLegalInterface } from '../../interfaces/profile/aspek-legal-interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -32,7 +34,7 @@ export class ProfileAspekLegalService {
   public getAspekLegal(){
     let token = this.authService.getLocalStorage('access_token')!;
     let party_id = this.authService.getLocalStorage('party_id')!;
-    let route = [party_id,"aspek_legal"]
+    let route = [party_id,"aspek_legal"];
     
     let api_get_aspek_legal: ApiInterface = {
       method: ApiRouteMethods.get,
@@ -44,5 +46,48 @@ export class ProfileAspekLegalService {
       }
     };
     return this.apiService.sendRequest(api_get_aspek_legal);
+  }
+
+  public addAspekLegal(params: AspekLegalInterface): Observable<any>{
+    let token = this.authService.getLocalStorage('access_token')!;
+    let party_id = this.authService.getLocalStorage('party_id')!;
+    let route = [party_id,"aspek_legal"];
+    let body: any = {
+      noAktaPendirian:params.noAktaPendirian,
+      tanggalTerbitAktaPendirian: params.tanggalTerbitAktaPendirian,
+      notarisAktaPendirian: params.notarisAktaPendirian,
+      notarisPenggantiAktaPendirian: params.notarisPenggantiAktaPendirian,
+      noAktaPerubahan: params.noAktaPerubahan,
+      tanggalTerbitAktaPerubahan: params.tanggalTerbitAktaPerubahan,
+      notarisAktaPerubahan: params.notarisAktaPerubahan,
+      notarisPenggantiAktaPerubahan:params.notarisPenggantiAktaPerubahan,
+      noSkPengesahanMenteri: params.noSkPengesahanMenteri,
+      tanggalTerbitNoSkPengesahanMenteri: params.tanggalTerbitNoSkPengesahanMenteri,
+      npwp: params.npwp,
+      tanggalTerbitNpwp: params.tanggalTerbitNpwp,
+      noSiup: params.noSiup,
+      tanggalTerbitSiup: params.tanggalTerbitSiup,
+      noNibTdp: params.noNibTdp,
+      isNiBTdpSeumurHidup: params.isNiBTdpSeumurHidup,
+      tanggalTerbitNibTdp: params.tanggalTerbitNibTdp,
+      tanggalExpireNibTdp: params.tanggalExpireNibTdp,
+      noIdpSitu: params.noIdpSitu,
+      isIdpSituSeumurHidup: params.isIdpSituSeumurHidup,
+      tanggalTerbitIdpSitu: params.tanggalTerbitIdpSitu,
+      tanggalExpireIdpSitu: params.tanggalExpireIdpSitu
+    }
+
+    let api_add_aspek_legal: ApiInterface = {
+      method: ApiRouteMethods.post,
+      url: ApiRoutes.api_vendor_route + route.join('/'),
+      body: body,
+      options: {
+        headers: {
+          Authorization: token,
+        }
+      }
+    };
+
+    return this.apiService.sendRequest(api_add_aspek_legal);
   }
 }
