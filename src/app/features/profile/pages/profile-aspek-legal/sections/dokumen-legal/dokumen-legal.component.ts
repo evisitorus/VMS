3a26c43fef68d39611116400bcbdad5359af793d
evadantir <evadantir@yahoo.com>
2022-04-05 15:39:36 +0700

    fixing id when put new file vms#365

--- src/app/features/profile/pages/profile-aspek-legal/sections/dokumen-legal/dokumen-legal.component.ts
+++ src/app/features/profile/pages/profile-aspek-legal/sections/dokumen-legal/dokumen-legal.component.ts
@@ -165,7 +165,7 @@ export class DokumenLegalComponent implements OnInit {
 
   public uploadDokLegal() {
     let params = {
-      id: this.dokData.id,
+      id: this.dokData.file.id,
       namaDokumen: this.dokLegalName,
       file: this.uploadedFileId,
       documentType: this.dokData.type,
